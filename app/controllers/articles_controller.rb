092ed462e8f419e996f0d762909a750f23455488
Aaron Briscoe <gmail2008@gmail.com>
2019-07-23 16:30:42 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,5 +19,20 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
-  # add edit and update methods here
+  def edit
+    @article = Article.find(params[:id])
+  
+  end
+
+  def update
+    @article = Article.new
+    @article.title = params[:title]
+    @article.description = params[:description]
+    @article.save
+    redirect_to "/articles/#{@article.id}"
+  end
+
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.destroy_all
+
+Article.create(title: "Article 1", description: "Description 1")
+Article.create(title: "Article 2", description: "Description 2")
+Article.create(title: "Article 3", description: "Description 3")
+Article.create(title: "Article 4", description: "Description 4")
